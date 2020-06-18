08eff06a8a0afc00ef6a8b67510f1a2f31e8bfef
Harsh Agrawal <agrawalharsh199925@gmail.com>
2020-06-18 20:11:34 +0530

    Assignment-1

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from 'reactstrap';
-
+import Dishdetail from './DishdetailComponent';
 class Menu extends Component{
 
     constructor(props){
@@ -9,31 +9,14 @@ class Menu extends Component{
            selectedDish:null,    
         };
     }
+    
     onDishSelect(dish){
         this.setState(
             {selectedDish:dish},
         );
     }
 
-    renderDish(dish){
-        if(dish != null){
-                return(
-                    <Card>
-                        <CardImg width="100%"  src={dish.image} alt={dish.name} />
-                        <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                                 <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-        }
-        else
-        {
-            return (
-                <div></div>
-            )
-        }
-    }
+    
 
     render(){
         const menu = this.props.dishes.map((dish) => {
@@ -51,11 +34,11 @@ class Menu extends Component{
         return(
                 <div className="container">
                     <div className="row">
-                        {menu }    
+                        {menu}    
                     </div>
-                    <div className="row">
-                        {this.renderDish(this.state.selectedDish)}
-                     </div>
+                    
+                        <Dishdetail dish={this.state.selectedDish}></Dishdetail>
+    
                 </div>
         );
     }
